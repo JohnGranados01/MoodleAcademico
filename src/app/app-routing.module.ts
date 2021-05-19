afbd461c6c0593869f2e1631de5dccbe4b175de4
JohnGranados <52686446+JohnGranados01@users.noreply.github.com>
2021-05-19 00:13:12 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./components/login/login.component";
-import { ViewMatterEstudentComponent } from "./components/view-matter-estudent/view-matter-estudent.component";
 import { ViewTaskEstudentComponent } from "./components/view-task-estudent/view-task-estudent.component";
 import { ViewMatterTeacherComponent } from "./components/view-matter-teacher/view-matter-teacher.component";
 import { QualifyTaskTeacherComponent } from "./components/qualify-task-teacher/qualify-task-teacher.component";
 
 const routes: Routes = [
   { path: "login", component: LoginComponent, pathMatch: "full" },
-  { path: "estudentMatterview", component:ViewMatterEstudentComponent,pathMatch:"full"},
   { path: "estudentTaskview", component:ViewTaskEstudentComponent,pathMatch:"full"},
   { path: "teacherMatterview", component:ViewMatterTeacherComponent,pathMatch:"full"},
   { path: "teacherTaskQualify", component:QualifyTaskTeacherComponent,pathMatch:"full"}
